c11879a4ee8253c045576fdb2b9c45efee6b96df
HuangMengsha <hms15992377064@163.com>
2023-09-15 11:08:36 +0800

    delete useless files

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ruangong_test {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ruangong_test to javafx.fxml;
-    exports com.example.ruangong_test;
-}
